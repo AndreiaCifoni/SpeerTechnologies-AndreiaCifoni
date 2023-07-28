ad2381d7d49cc1c8c0404a082e53fb1563f02cac
Andreia Cifoni <andreiacifoni@gmail.com>
2023-07-28 11:28:13 -0400

    changing BrowserRoute to HashRoute

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import App from "./App.jsx";
 
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename={"/"}>
+  <HashRouter basename={"/"}>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
