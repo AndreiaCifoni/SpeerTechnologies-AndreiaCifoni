96076b9c9897288b115468c4c0cfd6a257ad5034
Andreia Cifoni <andreiacifoni@gmail.com>
2023-07-28 10:46:18 -0400

    changes in webpack to include build

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
+  entry: "./src/index.js", // Entry point for webpack
   module: {
     rules: [
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
-            options: { minimize: true }
-          }
-        ]
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: "babel-loader",
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
-      filename: "./index.html"
-    })
-  ]
+      filename: "./index.html",
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+  },
 };
