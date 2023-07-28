508eecc684c5fe62783807776889424b33e5dbb4
Andreia Cifoni <andreiacifoni@gmail.com>
2023-07-28 09:23:16 -0400

    start deploying //  change app id for root

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-const root = createRoot(document.getElementById("app"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter basename={"/"}>
